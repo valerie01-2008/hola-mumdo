991d6883ea3f3fc6752d80cb72f0f29ac6ac86a2
valerie01-2008 <valerie.samper01@usa.edu.co>
2026-02-20 08:12:07 -0500

    Add files via upload

---
+++ comparar_madre.py
@@ -0,0 +1,9 @@
+edad_madre = 35
+tu_edad = 17
+
+if tu_edad > edad_madre:
+    print(f"Tienes {tu_edad} años. Eres mayor que tu madre ({edad_madre} años).")
+elif tu_edad == edad_madre:
+    print(f"Tienes {tu_edad} años. Tienes la misma edad que tu madre ({edad_madre} años).")
+else:
+    print(f"Tienes {tu_edad} años. Eres menor que tu madre ({edad_madre} años).")
